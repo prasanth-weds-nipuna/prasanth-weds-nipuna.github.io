b880dc78d9f1f80150c11adad490901c5b5a0306
prasanth-weds-nipuna <50958332+prasanth-weds-nipuna@users.noreply.github.com>
2019-06-05 01:58:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     new WOW().init();
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Jun 13, 2019 00:00:00").getTime();
+    var countDownDate = new Date("Jun 13, 2019 22:58:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
